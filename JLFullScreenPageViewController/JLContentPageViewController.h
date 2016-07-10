c1f0e05226941b1714ed0ef1df79d5762aadd400
Jangsy <jangsy7883@gmail.com>
2016-07-10 16:43:02 +0900

    소스정리

--- JLFullScreenPageViewController/JLContentPageViewController.h
+++ JLFullScreenPageViewController/JLContentPageViewController.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015년 Dalkomm. All rights reserved.
 //
 
-@import JLPageViewController;
+#import <JLPageViewController/JLPageViewController.h>
 
 @class JLContentPageViewController;
 
